ec501f77901400ee5fe799c184ddda07601d9a5b
kamitor <christiaan_gerardo@hotmail.com>
2017-06-27 22:00:10 +0200

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,8 +6,8 @@ set :default_stage, "production"
 set :ssh_options, {:forward_agent => true}
 
 set :application, 'capistrano_example'
-set :repo_url, 'git@github.com:davzie/capistrano-example.git'
-set :user, "davzie"
+set :repo_url, 'git@github.com:kamitor/capistrano-example.git'
+set :user, "root"
 
 set :linked_dirs, %w(my_shared_directory)
 
